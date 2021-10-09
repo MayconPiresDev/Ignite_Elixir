1eaba1a49478b78794a5e4ac5a6c021d98a4d574
Maycon Pires <mayconpires4@gmail.com>
2021-10-09 13:08:28 -0300

    Teste de mesa

--- sum_list/lib/sum_list.ex
+++ sum_list/lib/sum_list.ex
@@ -8,3 +8,11 @@ defmodule SumList do
     sum(tail, acc)
   end
 end
+
+# teste de mesa
+# [1,2,3], 0
+
+# 1 ex: [1,2,3] hd: 1, tail: [2,3], 0 -> acc = 0 + 1, sum([2,3], 1)
+# 2 ex: [2,3] hd: 2, tail: [3], 1, -> acc = 1 + 2, sum([3], 3)
+# 3 ex: [3] hd: 3, tail: [], 3 -> acc = 3 + 3, sum([], 6)
+# 4 ex: [] acc = 6
