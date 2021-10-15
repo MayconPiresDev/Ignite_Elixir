1210183e38be52a161a48aaa2615a9d0a5ff8fec
Maycon Pires <mayconpires4@gmail.com>
2021-10-15 11:22:37 -0300

    Criação do projeto challenge_reports_generator

---
+++ challenge_reports_generator/lib/challenge_reports_generator.ex
@@ -0,0 +1,18 @@
+defmodule ChallengeReportsGenerator do
+  @moduledoc """
+  Documentation for `ChallengeReportsGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChallengeReportsGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
