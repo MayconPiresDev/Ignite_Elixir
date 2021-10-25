b1ffc25027b9d75832e45ce663e96d7ec129fac7
Maycon Pires <mayconpires4@gmail.com>
2021-10-25 20:09:57 -0300

    Finalização test struct Item

--- exlivery/test/support/factory.ex
+++ exlivery/test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       cpf: "1234568900",
       age: 21,
       address: "Rua rua, 25"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de peperone",
+      category: :pizza,
+      quantity: 1,
+      unity_price: Decimal.new("28.90")
     }
   end
 end
