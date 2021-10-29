4a76b8c49b0d2cb80c9a483e77c982fe6814adfc
Maycon Pires <mayconpires4@gmail.com>
2021-10-28 22:27:51 -0300

    Facilitação de criação do User

--- exlivery/lib/exlivery.ex
+++ exlivery/lib/exlivery.ex
@@ -1,2 +1,10 @@
 defmodule Exlivery do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreatOrUpdate
+
+  def start_agents do
+    UserAgent.start_link(%{})
+  end
+
+  defdelegate creat_or_update_user(params), to: CreatOrUpdate, as: :call
 end
