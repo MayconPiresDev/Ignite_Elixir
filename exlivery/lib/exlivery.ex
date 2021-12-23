4678d3a30cb65eb1719bbd6e7bf7fecf9289eb92
Maycon Pires <piresmaycon4@gmail.com>
2021-12-23 10:53:35 -0300

    Finalização de criação do Agent-1

--- exlivery/lib/exlivery.ex
+++ exlivery/lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Users.CreatOrUpdate, as: CreatOrUpdateUser
   alias Exlivery.Users.Agent, as: UserAgent
-  alias Exlivery.Users.CreatOrUpdate
+  alias Exlivery.Orders.CreatOrUpdate, as: CreatOrUpdateOrders
 
   def start_agents do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
-  defdelegate creat_or_update_user(params), to: CreatOrUpdate, as: :call
+  defdelegate creat_or_update_user(params), to: CreatOrUpdateUser, as: :call
+  defdelegate creat_or_update_order(params), to: CreatOrUpdateOrders, as: :call
 end
